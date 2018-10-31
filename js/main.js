45d89c5bc037014277fd3112e4b69ac78cab35e3
Parimal Mehta <prmehta24@gmail.com>
2018-10-31 17:05:29 +0530

    Started Main 3 columns

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
 function goHome(){
 	location.href="index.html"
 }
+// var radioimages=['../images/Selena.jpg','../images/Coldplay.jpg','../images/DJSnake.jpg','../images/Enrique_Iglesias.jpg','../images/Pop.jpg','../images/Zayn_Malik.jpg'];
+// var radiotext=['Selena Gomez','ColdPlay','Dj Snake','Enrique Iglesias','Pop','Zayn Malik'];
